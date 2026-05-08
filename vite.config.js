09261ef2c428dd9605fd5f70424a1f97accc840a
ddrayko <189259481+ddrayko@users.noreply.github.com>
2026-05-08 16:36:43 +0200

    update the vite config for allow password-gen page

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         blur: resolve(__dirname, 'tools/blur/blur.html'),
-        correcteur: resolve(__dirname, 'tools/correcteur/correcteur.html')
+        correcteur: resolve(__dirname, 'tools/correcteur/correcteur.html'),
+        'password-gen': resolve(__dirname, 'tools/password-gen/password-gen.html')
       }
     }
   }
